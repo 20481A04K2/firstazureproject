020d93bfaa50c4f9625cc099979b21fb40990144
SAJJA VAMSI <99644384+20481A04K2@users.noreply.github.com>
2025-07-02 15:18:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Hello from Azure VM on port 8080!"
+    return "✅ Hello from Azure VM on port 8080! hello"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
